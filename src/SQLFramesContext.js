d22c2b27a15828903d8430f87227b0d260d453ac
Siva <siva.dirisala@gmail.com>
2022-07-07 21:25:40 -0700

    null is typescript compatible instead of {}

--- src/SQLFramesContext.js
+++ src/SQLFramesContext.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
 // to avoid storing the sqlframes dependency in window object
-const SQLFramesContext = React.createContext({});
+const SQLFramesContext = React.createContext(null);
 
 export default SQLFramesContext;
